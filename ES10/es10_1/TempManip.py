78d2e3f13b3e33f57996bf6df954b08d42754a51
NicolaBrancadori <86965627+NicolaBrancadori@users.noreply.github.com>
2021-07-08 14:33:34 +0200

    Delete TempManip.py

--- ES10/es10_1/TempManip.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with open("Temperatura.0") as Temp:
-	T=np.loadtxt(Temp,usecols=(0,1),dtype=float)
-
-
-print("Ho letto il file")
-# print("temperatura massima: ",T[:,0].max()," con un deltaL: ",T[:,1].max())
